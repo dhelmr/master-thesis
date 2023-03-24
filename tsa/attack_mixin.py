d7532c2bdfe116fec277ac79594ce774eaabcdc4
Daniel Helmrich <dh86hogi@studserv.uni-leipzig.de>
2023-03-24 10:29:40 +0100

    disable preprocessing because it takes too long

--- tsa/attack_mixin.py
+++ tsa/attack_mixin.py
@@ -144,10 +144,10 @@ class Experiment:
         n_gram_length = self._get_param("features", "n_gram_length", default=7)
         print(embedding, thread_aware, n_gram_length)
         ngram = Ngram([embedding], thread_aware, n_gram_length)
-        prepr = OutlierDetector(ngram)
+        # prepr = OutlierDetector(ngram)
 
         decision_engine_args = self._get_param("decision_engine", "args", default={}, exp_type=dict)
-        decision_engine = DecisionEngineClass(prepr, **decision_engine_args)
+        decision_engine = DecisionEngineClass(ngram, **decision_engine_args)
         if DecisionEngineClass == DECISION_ENGINES["Stide"]:
             decision_engine = StreamSum(decision_engine, False, 500, False)
         # decider threshold
