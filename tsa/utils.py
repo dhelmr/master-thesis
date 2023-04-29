a20305bf083c116104dff9b668eea443db210915
Daniel Helmrich <dh86hogi@studserv.uni-leipzig.de>
2023-04-29 19:46:15 +0200

    make building blocks definition more generic

--- tsa/utils.py
+++ tsa/utils.py
@@ -19,6 +19,29 @@ def random_permutation(items, k, nth_element, random_seed=0):
     return perm[:k]
 
 
+def access_cfg(root_obj, *keys, default=None, required=True, exp_type=None):
+    if default is not None:
+        required = False
+    cur_obj = root_obj
+    cur_key = ""
+    for key in keys:
+        if not isinstance(cur_obj, dict) and not isinstance(cur_obj, list):
+            raise ValueError("Parameter '%s' is not a dict or list." % cur_key)
+        if isinstance(cur_obj, dict) and key not in cur_obj:
+            if not required:
+                return default
+            else:
+                raise ValueError("Cannot find parameter for key %s at %s" % (key, cur_key))
+        cur_obj = cur_obj[key]
+        cur_key = "%s.%s" % (cur_key, key)
+    if exp_type is not None and not isinstance(cur_obj, exp_type):
+        raise ValueError("Parameter %s is not of expected type %s" % (cur_key, exp_type))
+    return cur_obj
+
+def exists_key(root_obj, *keys) -> bool:
+    val = access_cfg(root_obj, *keys, default=None, required=False)
+    return val is not None
+
 def choose_element(iterator, index):
     if index < 0:
         raise ValueError("index must be >= 0")
