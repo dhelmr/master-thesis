391f7333af2dbdf8e758aa2fe13684f0976e18bd
Daniel Helmrich <dh86hogi@studserv.uni-leipzig.de>
2023-05-10 10:54:10 +0200

    remove print lines

--- tsa/experiment.py
+++ tsa/experiment.py
@@ -39,7 +39,6 @@ class Experiment:
                                               **dataloader_config)
                 i = i + 1
                 if i < start_at:
-                    print("Skip", dataloader.__dict__)
                     continue
                 if dry_run:
                     print(i, "Dry Run: ", dataloader.__dict__)
