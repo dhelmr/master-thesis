f0ed08ea4f305c05568d113bae2f1d9fcf6aaec5
Daniel Helmrich <dh86hogi@studserv.uni-leipzig.de>
2023-05-12 12:53:50 +0200

    add tsa subcommand

---
+++ tsa/cli/tsa.py
@@ -0,0 +1,41 @@
+import os
+import tempfile
+from argparse import ArgumentParser
+
+import mlflow
+import pandas
+from mlflow import MlflowClient
+
+from tsa.cli.run import SubCommand, make_experiment
+from tsa.experiment_checker import ExperimentChecker
+
+
+class TSASubCommand(SubCommand):
+
+    def __init__(self):
+        super().__init__("tsa", "analyse training set experiments")
+    def make_subparser(self, parser: ArgumentParser):
+        parser.add_argument("--experiment", "-e", required=False, help="Sets the mlflow experiment ID", type=str)
+        parser.add_argument("-c", "--config", required=True, help="Experiment config yaml file.")
+        parser.add_argument("-o", "--output", required=True, help="Output file")
+    def exec(self, args, parser):
+        mlflow_client = MlflowClient() # TODO global singleton
+        experiment = make_experiment(args.config, mlflow_client, args.experiment)
+        checker = ExperimentChecker(experiment)
+        dfs = []
+        for run in checker.iter_mlflow_runs():
+            artifacts = experiment.mlflow.list_artifacts(run.info.run_id)
+            for a in artifacts:
+                if str(a.path).endswith(".parquet.gz"):
+                    tmpfile = tempfile.mktemp()
+                    print("Download...", run.info.run_id, a.path)
+                    mlflow.artifacts.download_artifacts(run_id=run.info.run_id, artifact_path=a.path, dst_path=tmpfile)
+                    # local_file = os.path.join(tmpfile, str(a.path))
+                    df = pandas.read_parquet(tmpfile)
+                    df["run_id"] = run.info.run_id
+                    for col in run.data.params:
+                        df[col] = run.data.params[col]
+                    # df = df.append(run.data.params, ignore_index=True)
+                    dfs.append(df)
+        big_df = pandas.concat(dfs, axis=0)
+        big_df.to_csv(args.output)
