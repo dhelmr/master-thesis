61c1bfc571ac2e67cf62bf9c73a8f6d39a6182b2
Daniel Helmrich <dh86hogi@studserv.uni-leipzig.de>
2023-06-07 12:57:39 +0200

    wip

---
+++ tsa/dataloaders/combination_dl.py
@@ -0,0 +1,74 @@
+from typing import List
+
+from dataloader.base_data_loader import BaseDataLoader
+
+def yield_successively(lists):
+    cur_indexes = [0] * len(lists)
+    yielded_one = True
+    while yielded_one:
+        yielded_one = False
+        for i, l in enumerate(lists):
+            if cur_indexes[i] >= len(l):
+                continue
+            yield l[cur_indexes[i]]
+            cur_indexes[i] += 1
+            yielded_one = True
+class CombinationDL(BaseDataLoader):
+    def __init__(self, training_dl: List[BaseDataLoader],
+                 validation_dl: List[BaseDataLoader],
+                 test_dl: List[BaseDataLoader], scenario_path: str):
+        super().__init__(scenario_path)
+        self._train = training_dl
+        self._validation_dl = validation_dl
+        self._test_dl = test_dl
+
+    def _combine(self, recordings: list) -> list:
+        return list(yield_successively(recordings))
+
+    def training_data(self) -> list:
+        return self._combine([dl.training_data() for dl in self._train])
+
+    def validation_data(self) -> list:
+        return self._combine([dl.validation_data() for dl in self._validation_dl])
+
+    def test_data(self) -> list:
+        return self._combine([dl.test_data() for dl in self._test_dl])
+
+    def _iter_distinct_dataloaders(self):
+        iterated_dataloaders = []
+        for dl_group in [self._train, self._validation_dl, self._test_dl]:
+            for dl in dl_group:
+                if dl in iterated_dataloaders:
+                    continue
+                yield dl
+                iterated_dataloaders.append(dl)
+
+
+    def extract_recordings(self, category: str) -> list:
+        raise NotImplementedError("not implemented")
+
+    def collect_metadata(self) -> dict:
+        aggregated_metadata = {}
+        for dl in self._iter_distinct_dataloaders():
+            aggregated_metadata.update(dl.collect_metadata())
+        return aggregated_metadata
+
+    def get_direction_string(self):
+        direction_str = None
+        for dl in self._iter_distinct_dataloaders():
+            cur_direction_str = dl.get_direction_string()
+            if direction_str is None:
+                direction_str = cur_direction_str
+            elif direction_str != cur_direction_str:
+                raise RuntimeError("Direction strings of the dataloaders do not match! %s != %s" % (cur_direction_str, direction_str))
+        return direction_str
+
+    def cfg_dict(self):
+        # TODO implement own superclass for dataloaders
+        #parent_dict = deepcopy(self.dl.cfg_dict())
+        #parent_dict["max_syscalls"] = self._max_syscalls
+        aggregated_cfg = {}
+        for dl in self._iter_distinct_dataloaders():
+            aggregated_cfg.update(dl.cfg_dict())
+        return aggregated_cfg
+
