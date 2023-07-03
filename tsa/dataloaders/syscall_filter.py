48491015f3f14288dea63f4466cc96f1c4bae680
Daniel Helmrich <dh86hogi@studserv.uni-leipzig.de>
2023-07-03 16:40:06 +0200

    fix syscall counter

--- tsa/dataloaders/syscall_filter.py
+++ tsa/dataloaders/syscall_filter.py
@@ -16,8 +16,13 @@ class MaxSyscallFilter(SyscallFilter):
         self._syscall_counter = 0
 
     def filter(self, syscall: Syscall) -> bool:
-        self._syscall_counter += 1
         if self._max_syscalls is None:
+            self._syscall_counter += 1
             return True
-        filter = self._syscall_counter <= self._max_syscalls
-        return filter
+
+        do_filter = self._syscall_counter < self._max_syscalls
+        if do_filter:
+            # only increment the counter if the syscall is passed on
+            # this way, the counter will never be greater than self._max_syscalls
+            self._syscall_counter += 1
+        return do_filter
