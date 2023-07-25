224159e6f25c8ab0d15a57212e8d2d3884356a9f
Daniel Helmrich <dh86hogi@studserv.uni-leipzig.de>
2023-07-25 15:31:59 +0200

    add pca and pdists stats analysing

--- tsa/analysis/ngram_thread_analyser.py
+++ tsa/analysis/ngram_thread_analyser.py
@@ -1,14 +1,20 @@
 import numpy as np
-from numpy import e
+from scipy.spatial.distance import pdist
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import MinMaxScaler
 
 from tsa.analysis.analyser import AnalyserBB
 from tsa.ngram_thread_pca import NgramThreadMatrix
 
 
 class NgramThreadAnalyser(AnalyserBB):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, eigenvalues=False, matrix_norms=False, pdists=False, pca=False, **kwargs):
         super().__init__(*args, **kwargs)
         self.ngram_thread_matrix = NgramThreadMatrix()
+        self.eigenvalues = eigenvalues
+        self.matrix_norms = matrix_norms
+        self.pdists = pdists
+        self.pca = pca
 
     def _add_input(self, syscall, inp):
         if inp is None:
@@ -38,8 +44,11 @@ class NgramThreadAnalyser(AnalyserBB):
                                      n_classes=unique_ngrams)
 
         self._add_matrix_stats(stats, "ngramXthreads", matrix)
-        self._add_matrix_stats(stats, "ngram_distances", self.ngram_thread_matrix.ngram_distances())
-        self._add_matrix_stats(stats, "thread_distances", self.ngram_thread_matrix.thread_distances())
+        if self.pdists:
+            self._add_pdist_stats(stats, "ngram_dists", matrix)
+            self._add_pdist_stats(stats, "thread_dists", np.transpose(matrix))
+        if self.pca:
+            self._add_pca_stats(stats, "ngramXthreads_pca", matrix)
         return [stats]
 
     def _add_distribution_stats(self, stats, prefix: str, dists, n_classes):
@@ -61,31 +70,34 @@ class NgramThreadAnalyser(AnalyserBB):
                                      items=[dist.unique_elements() for _, dist in dists.items()])
 
     def _update_deviation_stats(self, stats, prefix, items):
+        v = np.var(items)
+        m = np.mean(items)
         stats.update({
-            "%s_mean" % prefix: np.mean(items),
-            "%s_var" % prefix: np.var(items),
+            "%s_mean" % prefix: m,
+            "%s_var" % prefix: v,
             "%s_min" % prefix: np.min(items),
             "%s_max" % prefix: np.max(items),
             "%s_median" % prefix: np.median(items),
+            "%s_iod" % prefix: v / m
         })
 
     def _add_matrix_stats(self, stats, prefix, matrix):
-        matrix_stats = {
-            "mean": np.mean(matrix),
-            "var": np.var(matrix),
-            "std": np.std(matrix)
-        }
-        nuc_norm = np.linalg.norm(matrix, ord="nuc")
-        matrix_stats["(nuc)-norm"] = nuc_norm
-        for norm_ord in ["fro", 1, -1, 2, -2]:
-            norm = np.linalg.norm(matrix, ord=norm_ord)
-            matrix_stats["(%s)-norm" % norm_ord] = norm
-            matrix_stats["(%s)-norm/nuc-norm" % norm_ord] = norm/nuc_norm
+        self._update_deviation_stats(stats, prefix, matrix)
+        matrix_stats = {}
+        if self.matrix_norms:
+            nuc_norm = np.linalg.norm(matrix, ord="nuc")
+            matrix_stats["(nuc)-norm"] = nuc_norm
+            for norm_ord in ["fro", 1, -1, 2, -2]:
+                norm = np.linalg.norm(matrix, ord=norm_ord)
+                matrix_stats["(%s)-norm" % norm_ord] = norm
+                matrix_stats["(%s)-norm/nuc-norm" % norm_ord] = norm / nuc_norm
         matrix_stats = {"%s-%s" % (prefix, key): v for key, v in matrix_stats.items()}
         stats.update(matrix_stats)
         self._add_eigenval_stats(stats, prefix, matrix)
 
     def _add_eigenval_stats(self, stats, prefix, matrix):
+        if not self.eigenvalues:
+            return
         matrix = np.array(matrix)
         cov = np.cov(matrix)
         eigenvals = np.linalg.eigvals(cov)
@@ -96,3 +108,24 @@ class NgramThreadAnalyser(AnalyserBB):
         stats["%s_eigenval_mean_5" % prefix] = np.mean(sorted_vals[:5])
         stats["%s_eigenval_mean_10" % prefix] = np.mean(sorted_vals[:10])
         stats["%s_eigenval_mean_20" % prefix] = np.mean(sorted_vals[:20])
+
+    def _add_pdist_stats(self, stats, prefix, matrix):
+        for metric in ["jensenshannon", "jaccard", "correlation", "cosine", "canberra", "euclidean", "braycurtis"]:
+            distance_matrix = pdist(matrix, metric)
+            self._add_matrix_stats(stats, prefix=f"{prefix}-{metric}", matrix=distance_matrix)
+
+    def _add_pca_stats(self, stats, prefix, matrix):
+        pca = PCA(n_components=10)
+        minmax = MinMaxScaler()
+        scaled = minmax.fit_transform(matrix)
+        pca.fit_transform(scaled)
+
+        pca_stats = {"pca-noise-var": pca.noise_variance_,}
+        for i, evr in enumerate(pca.explained_variance_ratio_):
+            pca_stats[f"pca-evr-n{i}"] = evr
+        for i, ev in enumerate(pca.explained_variance_):
+            pca_stats[f"pca-ev-n{i}"] = ev
+        pca_stats["pca-ev-sum"] = sum(pca.explained_variance_)
+        pca_stats["pca-evr-sum"] = sum(pca.explained_variance_ratio_)
+
+        stats.update({"%s-%s" % (prefix, k) : v for k,v in pca_stats.items()})
