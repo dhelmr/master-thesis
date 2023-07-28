000e8ad92faabcd6f81173e3b460f546ed38974d
Daniel Helmrich <dh86hogi@studserv.uni-leipzig.de>
2023-07-28 13:55:51 +0200

    Add relative frequency filter

--- tsa/unsupervised/frequency_od.py
+++ tsa/unsupervised/frequency_od.py
@@ -3,11 +3,23 @@ from tsa.unsupervised.preprocessing import OutlierDetector
 
 class FrequencyOD(OutlierDetector):
 
-    def __init__(self, building_block, train_features=None, threshold = 3):
+    def __init__(self, building_block, train_features=None, threshold=3, relative=False):
         super().__init__(building_block, train_features)
+        self._relative = relative
         self._threshold = threshold
 
+    def _get_threshold(self, training_data):
+        if self._relative:
+            len_td = len(training_data)
+            t = self._threshold * len_td
+            print(f"Set threshold to {self._threshold} of training data (size={len_td}) => t={t}")
+            return t
+        else:
+            return self._threshold
+
     def detect_anomalies(self, training_data):
+        threshold = self._get_threshold(training_data)
+        print(f"Remove all ngrams with frequency < {threshold}")
         counts = {}
         for i, t in training_data:
             if t not in counts:
@@ -15,6 +27,6 @@ class FrequencyOD(OutlierDetector):
             counts[t] += 1
         anomalies = set()
         for i, t in training_data:
-            if counts[t] <= self._threshold:
+            if counts[t] <= threshold:
                 anomalies.add(i)
         return anomalies
