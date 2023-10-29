2be8c8557711cb4ce00db3842111898a2cb839ec
Daniel Helmrich <dh86hogi@studserv.uni-leipzig.de>
2023-10-29 09:23:57 +0100

    add models_per_trace

--- tsa/MicroSTIDEs.py
+++ tsa/MicroSTIDEs.py
@@ -11,7 +11,7 @@ from tsa.ngram_thread_matrix import process_thread_id
 AGGREGATIONS = ["mean", "median"]
 class MicroSTIDEs(BuildingBlock):
 
-    def __init__(self, input: BuildingBlock, n_models=20, stream_sum_window_len = 1000, aggregation="mean"):
+    def __init__(self, input: BuildingBlock, n_models=20, models_per_trace=1, stream_sum_window_len = 1000, aggregation="mean"):
         super().__init__()
         # parameter
         self._input = input
@@ -20,6 +20,7 @@ class MicroSTIDEs(BuildingBlock):
         self._n_models = n_models
         self._normal_models = [set() for _ in range(n_models)]
         self._trace_ids = {}
+        self._models_per_trace = models_per_trace
 
         self._stream_sums = [deque(maxlen=stream_sum_window_len) for _ in range(n_models)]
 
@@ -34,12 +35,12 @@ class MicroSTIDEs(BuildingBlock):
     def depends_on(self):
         return self._dependency_list
 
-    def _get_model(self, syscall):
+    def _get_models(self, syscall):
         trace_id = process_thread_id(syscall)
         if trace_id not in self._trace_ids:
-            self._trace_ids[trace_id] = random.randrange(self._n_models)
-        model_id = self._trace_ids[trace_id]
-        return self._normal_models[model_id]
+            self._trace_ids[trace_id] = random.choices(range(self._n_models), k=self._models_per_trace)
+        for model_id in self._trace_ids[trace_id]:
+            yield self._normal_models[model_id]
 
     def train_on(self, syscall: Syscall):
         """
@@ -48,8 +49,8 @@ class MicroSTIDEs(BuildingBlock):
         ngram = self._input.get_result(syscall)
         if ngram is None:
             return
-        model = self._get_model(syscall)
-        model.add(ngram)
+        for model in self._get_models(syscall):
+            model.add(ngram)
 
     def fit(self):
         pass
