9a9be596f9d1f62f9d7a1f96a3d9bb3aa617da64
Daniel Helmrich <dh86hogi@studserv.uni-leipzig.de>
2023-11-09 13:16:41 +0100

    remove comment

--- tsa/utils.py
+++ tsa/utils.py
@@ -92,7 +92,6 @@ def gini_coeff(x):
     sorted_x = np.sort(x)
     n = len(x)
     cumx = np.cumsum(sorted_x, dtype=float)
-    # The above formula, with all weights equal to 1 simplifies to:
     return (n + 1 - 2 * np.sum(cumx) / cumx[-1]) / n
 
 def md5(*args):
