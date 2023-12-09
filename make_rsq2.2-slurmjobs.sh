c70d6e2d99dd0cec835ba6913f8999dcc2ba97ed
Daniel Helmrich <dh86hogi@studserv.uni-leipzig.de>
2023-12-09 12:56:05 +0100

    Use clara long cluster

--- make_rsq2.2-slurmjobs.sh
+++ make_rsq2.2-slurmjobs.sh
@@ -4,7 +4,7 @@ SKIP_FEATURES="ratio_unseen_test_ngrams jensen_shannon_distance jensen_shannon_d
 mkdir -p results/rsq2-2/fs/
 
 function run { # args: [feature file] [max depth of decision tree] [total number of features to select] [additional args]
-SLURM_MEM_GB=30 SLURM_HOURS=48 slurm/run.sh tsa-fs -i results/rsq2-2/$1.csv -o results/rsq2-2/fs/$1-depth=$2rounds=$3.csv -p DecisionTree --skip-features $SKIP_FEATURES --max-depth $2 --minmax-scaling --total $3 $4
+SLURM_CLUSTER=clara-long SLURM_MEM_GB=30 SLURM_HOURS=60 slurm/run.sh tsa-fs -i results/rsq2-2/$1.csv -o results/rsq2-2/fs/$1-depth=$2rounds=$3.csv -p DecisionTree --skip-features $SKIP_FEATURES --max-depth $2 --minmax-scaling --total $3 $4
 }
 
 function iterdepth {
